e954c0d2b18859ee69c4ef625f5e8469b8b30f6d
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-12-02 14:12:45 -0300

    feat: Routes explicação e implementação.

--- memoteca/src/app/app-routing.module.ts
+++ memoteca/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listarPensamento',
+    pathMatch: 'full',
+  },
+  {
+    path: 'listarPensamento',
+    component: ListarPensamentoComponent,
+  },
+  {
+    path: 'criarPensamento',
+    component: CriarPensamentoComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
