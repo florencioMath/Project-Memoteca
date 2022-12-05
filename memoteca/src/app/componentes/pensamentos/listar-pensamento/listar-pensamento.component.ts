73c6296e56ad4a41b9ebe7b1848a3b52f0a8ca86
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-12-05 15:19:04 -0300

    feat: ngClass introdução e exemplo.

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -17,5 +17,11 @@ export class ListarPensamentoComponent {
       autoria: 'Componente Filho',
       modelo: 'modelo2',
     },
+    {
+      conteudo:
+        'To see a World in a Grain of Sand, And a Heaven in a Wild Flower, Hold Infinity in the palm of your hand, And Eternity in an hour, A Robin Red breast in a Cage, Puts all Heaven in a Rage, A Dove house filld with Doves & Pigeons, Shudders Hell thr all its regions ',
+      autoria: 'Auguries of Innocence by Wiliam Blake',
+      modelo: 'modelo3',
+    },
   ];
 }
