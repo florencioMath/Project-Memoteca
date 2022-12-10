97036f4468f32c9d0640d900686b2015f2efc372
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-12-10 13:48:46 -0300

    feat: HttpClient introdução e exemplo.

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,11 +1,17 @@
+import { PensamentoService } from './../pensamento.service';
 import { Pensamento } from './../pensamento';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
   styleUrls: ['./listar-pensamento.component.css'],
 })
-export class ListarPensamentoComponent {
+export class ListarPensamentoComponent implements OnInit {
   listaPensamentos: Pensamento[] = [];
+  constructor(private service: PensamentoService) {}
+
+  ngOnInit(): void {
+    this.service.listar();
+  }
 }
--- memoteca/src/app/componentes/pensamentos/pensamento.service.ts
+++ memoteca/src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,3 +1,4 @@
+import { Pensamento } from './pensamento';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -5,5 +6,10 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class PensamentoService {
+  private readonly API = 'http://localhost:3000/pensamentos';
   constructor(private http: HttpClient) {}
+
+  listar() {
+    return this.http.get<Pensamento[]>(this.API);
+  }
 }
