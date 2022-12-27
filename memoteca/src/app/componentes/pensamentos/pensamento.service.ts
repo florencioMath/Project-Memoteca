e35005b95046b9fd9b554c1f105cd76641632d7f
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-12-27 10:27:45 -0300

    feat: Pesquisando pensamentos.

--- memoteca/src/app/componentes/pensamentos/pensamento.service.ts
+++ memoteca/src/app/componentes/pensamentos/pensamento.service.ts
@@ -9,13 +9,20 @@ import { enviroment } from 'src/environments/enviroment';
 })
 export class PensamentoService {
   private readonly API = enviroment.API;
+
   constructor(private http: HttpClient) {}
 
-  listar(pagina: number): Observable<Pensamento[]> {
+  listar(pagina: number, filtro: string): Observable<Pensamento[]> {
     const itensPorPagina = 6;
+
     let params = new HttpParams()
       .set('_page', pagina)
       .set('_limit', itensPorPagina);
+
+    if (filtro.trim().length > 2) {
+      params = params.set('q', filtro);
+    }
+
     return this.http.get<Pensamento[]>(this.API, { params });
   }
 
