e6d6bbe194fe2f74e990cecd516c58b55e932d39
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-12-27 21:31:13 -0300

    feat: Paginação e buscar nos favoritos.

--- memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -18,6 +18,8 @@ export class PensamentoComponent {
     favorito: false,
   };
 
+  @Input() listaFavoritos: Pensamento[] = [];
+
   larguraPensamento(): string {
     if (this.pensamento.conteudo.length > 256) {
       return 'pensamento-g';
@@ -33,6 +35,12 @@ export class PensamentoComponent {
   }
 
   atualizarFavorito() {
-    this.service.mudarFavorito(this.pensamento).subscribe();
+    this.service.mudarFavorito(this.pensamento).subscribe(() => {
+      this;
+      this.listaFavoritos.splice(
+        this.listaFavoritos.indexOf(this.pensamento),
+        1
+      );
+    });
   }
 }
