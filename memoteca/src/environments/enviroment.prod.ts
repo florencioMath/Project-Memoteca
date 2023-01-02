0c4ed20d8115ae73b5b7573488a7ee8e3d8f6021
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-01-02 14:54:03 -0300

    feat: Update na URL da API para o deploy no Firebase.

--- memoteca/src/environments/enviroment.prod.ts
+++ memoteca/src/environments/enviroment.prod.ts
@@ -1,4 +1,4 @@
 export const enviroment = {
   production: false,
-  API: 'https://raw.githubusercontent.com/florencioMath/Project-Memoteca/main/memoteca/backend/db.json',
+  API: 'http://localhost:3000/pensamentos',
 };
